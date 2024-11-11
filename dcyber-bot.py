090e42d155fd7b07f32de1cc5c87d1e4914f3790
diegocamara89 <marcelodiego@gmail.com>
2024-11-10 21:08:53 -0300

    Update dcyber-bot.py

--- dcyber-bot.py
+++ dcyber-bot.py
@@ -378,6 +378,7 @@ def main():
     criar_tabela_acessos()
     criar_todas_tabelas()
     criar_tabela_estatisticas()
+    criar_tabela_contatos()
     
     # Criar tabelas específicas
     conn = get_db_connection()
